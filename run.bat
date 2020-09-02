634db4b8e2ca2862acaaa8135158404de89de6c5
Shailendra Verma <shailendravermag@gmail.com>
2020-09-02 13:46:07 +0100

    update

--- run.bat
+++ run.bat
@@ -1,2 +1,2 @@
 rem %1 is to pass additional parameter to mvn cmd like -o for offline
-mvn exec:java -D"exec.mainClass"="com.sv.bigfile.SearchBigFile" %1
+mvn exec:java -D"exec.mainClass"="com.sv.runcmd.RunCommand" %1
