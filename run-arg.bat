4dc843046292ea839bfaa2bccc34edf03fa6bc99
Shailendra Verma <shailendravermag@gmail.com>
2020-09-17 21:38:50 +0100

    update config

--- run-arg.bat
+++ run-arg.bat
@@ -1,3 +1,2 @@
 rem %1 is to pass additional parameter to mvn cmd like -o for offline
-rem mvn exec:java -D"exec.mainClass"="com.sv.runcmd.RunCommand" -D"exec.args"="'d:\java-prg\ScreenCheck\run.bat'" %1
-mvn exec:java -Dexec.mainClass="com.sv.runcmd.RunCommand" -Dexec.args="'*c:\sv\cmds\search.bat (search)'" %1
+mvn exec:java -D"exec.mainClass"="com.sv.runcmd.RunCommand" -D"exec.args"="'d:\java-prg\sc.bat'" %1
