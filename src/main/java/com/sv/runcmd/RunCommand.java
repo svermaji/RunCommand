9fc21e80bac493688d3946c10c8c6a8629c27b14
Shailendra Verma <shailendravermag@gmail.com>
2021-09-14 10:22:21 +0100

    update to use as lib

--- src/main/java/com/sv/runcmd/RunCommand.java
+++ src/main/java/com/sv/runcmd/RunCommand.java
@@ -3,30 +3,36 @@ package com.sv.runcmd;
 import com.sv.core.Utils;
 import com.sv.core.logger.MyLogger;
 
-import java.io.BufferedReader;
-import java.io.IOException;
-import java.io.InputStreamReader;
-
 public class RunCommand {
 
-    private final MyLogger logger;
+    private MyLogger logger;
     private final RunCommandUtil commandUtil;
 
     public static void main(String[] args) {
         new RunCommand(args);
     }
 
-    public RunCommand(String[] args) {
+    public RunCommand(String[] args, MyLogger loggerObj) {
+
+        this.logger = loggerObj;
         if (args != null && args.length == 1) {
-            logger = MyLogger.createLogger("run-cmd-arg.log");
+            if (logger == null) {
+                this.logger = MyLogger.createLogger("run-cmd-arg.log");
+            }
             commandUtil = new RunCommandUtil(logger);
             commandUtil.execCommand(args[0]);
             Utils.sleep1Sec();
         } else {
-            logger = MyLogger.createLogger("run-cmd.log");
+            if (loggerObj == null) {
+                this.logger = MyLogger.createLogger("run-cmd.log");
+            }
             commandUtil = new RunCommandUtil(logger);
             new RunCommandUI(logger);
         }
+    }
+
+    public RunCommand(String[] args) {
+        this(args, null);
     }
 }
 
